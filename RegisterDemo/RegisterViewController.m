4d4373039d837908ab4dace430d948c3bb680a62
浅爱 <shallowlove21@gmail.com>
2018-03-19 08:56:23 +0800

    commit

--- RegisterDemo/RegisterViewController.m
+++ RegisterDemo/RegisterViewController.m
@@ -1,33 +1,31 @@
 //
-//  ViewController.m
+//  RegisterViewController.m
 //  RegisterDemo
 //
 //  Created by PatrickY on 2018/3/16.
 //  Copyright © 2018年 PatrickY. All rights reserved.
 //
 
-#import "ViewController.h"
 #import "RegisterViewController.h"
 
-@interface ViewController ()
+@interface RegisterViewController ()
 
 @property (nonatomic, strong) UILabel                           *usernameLabel;
 @property (nonatomic, strong) UILabel                           *passwordLabel;
 @property (nonatomic, strong) UITextField                       *usernameTextField;
 @property (nonatomic, strong) UITextField                       *passwordTextField;
-@property (nonatomic, strong) UIButton                          *loginButton;
 @property (nonatomic, strong) UIButton                          *registerButton;
 
 @end
 
-@implementation ViewController
+@implementation RegisterViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     CGRect screen = [UIScreen mainScreen].bounds;
     
-    self.navigationItem.title = @"登录";
+    self.navigationItem.title = @"注册";
     CGFloat labelWidth = 60;
     CGFloat labelHeight = 30;
     CGFloat textFieldWidth = 200;
@@ -42,7 +40,7 @@
     self.usernameLabel = [[UILabel alloc] initWithFrame:CGRectMake(leftView, topView, labelWidth, labelHeight)];
     self.usernameLabel.text = @"用户名:";
     self.usernameLabel.textAlignment = NSTextAlignmentRight;
- 
+    
     self.passwordLabel = [[UILabel alloc] initWithFrame:CGRectMake(leftView, topView + marginView, labelWidth, labelHeight)];
     self.passwordLabel.text = @"密码:";
     self.passwordLabel.textAlignment = NSTextAlignmentRight;
@@ -56,52 +54,47 @@
     self.passwordTextField.clearButtonMode = UITextFieldViewModeWhileEditing;
     self.passwordTextField.secureTextEntry = YES;
     
-    self.loginButton = [UIButton buttonWithType:UIButtonTypeSystem];
-    self.loginButton.frame = CGRectMake((screen.size.width - buttonWidth) / 2, 300, buttonWidth, buttonHeight);
-    [self.loginButton setTitle:@"登录" forState:UIControlStateNormal];
     
     self.registerButton = [UIButton buttonWithType:UIButtonTypeSystem];
     self.registerButton.frame = CGRectMake((screen.size.width - buttonWidth) / 2, marginView + 300, buttonWidth, buttonHeight);
     [self.registerButton setTitle:@"注册" forState:UIControlStateNormal];
     [self.registerButton addTarget:self action:@selector(registerButtonClick) forControlEvents:UIControlEventTouchUpInside];
     
+    UIBarButtonItem *saveButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemSave target:self action:@selector(registerButtonClick)];
+    UIBarButtonItem *cancelButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:self action:@selector(cancel)];
+    
+    self.navigationItem.rightBarButtonItem = saveButtonItem;
+    self.navigationItem.leftBarButtonItem = cancelButtonItem;
+    
     [self.view addSubview:self.usernameLabel];
     [self.view addSubview:self.passwordLabel];
     [self.view addSubview:self.usernameTextField];
     [self.view addSubview:self.passwordTextField];
-    [self.view addSubview:self.loginButton];
     [self.view addSubview:self.registerButton];
     
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(registerCompleted:) name:@"RegisterCompletedNotification" object:nil];
     
 }
-
-//注册按钮点击事件
-- (void)registerButtonClick {
-    
-    RegisterViewController *registerViewController = [[RegisterViewController alloc] init];
-    UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:registerViewController];
-    
-    [self presentViewController:navController animated:YES completion:nil];
-    
-}
-
-- (void)registerCompleted:(NSNotification *)notification {
-    
-    NSDictionary *dataDict = [notification userInfo];
-    NSString *username = dataDict[@"username"];
-    NSString *password = dataDict[@"password"];
-    self.usernameTextField.text = username;
-    self.passwordTextField.text = password;
-    NSLog(@"%@ -- %@", username, password);
-}
-
 
 - (void)didReceiveMemoryWarning {
-    
     [super didReceiveMemoryWarning];
     
-    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (void)registerButtonClick {
+    
+    [self dismissViewControllerAnimated:YES completion:^{
+        NSLog(@"注册回调");
+        
+        NSDictionary *dataDict = @{@"username":self.usernameTextField.text, @"password":self.passwordTextField.text};
+        [[NSNotificationCenter defaultCenter] postNotificationName:@"RegisterCompletedNotification" object:nil userInfo:dataDict];
+        
+    }];
+}
+
+- (void)cancel {
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
 }
 
 @end
